97d9221ca49ccaae879a2bf5f1262ba7ba3a6bbb
Yonathan      aka (witwizard,orlserg) <109464957+yonathanpy@users.noreply.github.com>
2026-04-19 18:27:32 +0000

    Create ioc_reporter.c

---
+++ ioc_reporter.c
@@ -0,0 +1,9 @@
+#include "ioc_reporter.h"
+#include <stdio.h>
+
+void generate_ioc_report() {
+    printf("[IOC_REPORT] generating indicators of compromise\n");
+
+    printf("Hash sample: SHA256: XXXX\n");
+    printf("Suspicious file pattern: detected\n");
+}
