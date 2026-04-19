15515440b7a84eb3a96b3891fbbeb0398fd408b6
Yonathan      aka (witwizard,orlserg) <109464957+yonathanpy@users.noreply.github.com>
2026-04-19 18:32:07 +0000

    Create behavioral_event_correlation.c

---
+++ behavioral_event_correlation.c
@@ -0,0 +1,6 @@
+#include "behavioral_event_correlation.h"
+
+void correlate_events() {
+    printf("[EVENT_CORRELATION] mapping system events to behavioral clusters\n");
+    printf("[EVENT_CORRELATION] anomaly scoring applied across execution timeline\n");
+}
