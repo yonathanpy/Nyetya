ae562165d5ed42c0e967212ee42a0cafd53485bc
Yonathan      aka (witwizard,orlserg) <109464957+yonathanpy@users.noreply.github.com>
2026-04-19 18:32:21 +0000

    Create artifact_extraction_module.c

---
+++ artifact_extraction_module.c
@@ -0,0 +1,6 @@
+#include "artifact_extraction_module.h"
+
+void extract_artifacts() {
+    printf("[ARTIFACT_EXTRACTION] scanning memory and disk residue patterns\n");
+    printf("[ARTIFACT_EXTRACTION] extracting forensic indicators of execution\n");
+}
